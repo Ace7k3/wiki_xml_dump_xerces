7083b0ba59b61674a3f6a359f42272889a3569ff
Benjamin Cabrera <benjamin.cabrera@uni-due.de>
2017-02-23 14:56:49 +0100

    Added and refactored some title filters

--- src/handlers/basicTitleFilters.hpp
+++ src/handlers/basicTitleFilters.hpp
@@ -15,11 +15,29 @@ namespace WikiXmlDumpXerces {
 					|| title.substr(0,10) == "User talk:"
 					|| title.substr(0,10) == "File talk:"
 					|| title.substr(0,15) == "Wikipedia talk:"
+					|| title.substr(0,5) == "Talk:"
 					);
 		};
 	}
 
-	inline std::function<bool(const std::string&)> only_talk_pages() {
+	inline std::function<bool(const std::string&)> only_articles_and_categories() {
+		return [](const std::string& title) {
+			return !(
+					title.substr(0,5) == "User:" 
+					|| title.substr(0,10) == "Wikipedia:" 
+					|| title.substr(0,5) == "File:" 
+					|| title.substr(0,14) == "Category talk:" 
+					|| title.substr(0,14) == "Template talk:"
+					|| title.substr(0,9) == "Template:"
+					|| title.substr(0,10) == "User talk:"
+					|| title.substr(0,10) == "File talk:"
+					|| title.substr(0,15) == "Wikipedia talk:"
+					|| title.substr(0,5) == "Talk:"
+					);
+		};
+	}
+
+	inline std::function<bool(const std::string&)> only_article_talk_pages() {
 		return [](const std::string& title) {
 			return title.substr(0,5) == "Talk:";
 		};
