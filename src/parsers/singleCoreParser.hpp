735c7d1741425700f016d98620a83ca90bf20e25
Benjamin Cabrera <benjamin.cabrera@uni-due.de>
2017-04-26 14:54:08 +0200

    Added RunOnString to single core parser

--- src/parsers/singleCoreParser.hpp
+++ src/parsers/singleCoreParser.hpp
@@ -6,6 +6,8 @@
 #include <xercesc/sax2/SAX2XMLReader.hpp>
 #include <xercesc/sax2/XMLReaderFactory.hpp>
 #include <xercesc/sax2/DefaultHandler.hpp>
+#include <xercesc/framework/MemBufInputSource.hpp>
+// #include <xercesc/util/Base64.hpp>
 
 #include "../handlers/abstractWikiPageHandler.hpp"
 #include "../handlers/wikiDumpXercesHandler.hpp"
@@ -36,6 +38,26 @@ namespace WikiXmlDumpXerces {
 
 				// run parser
 				parser->parse(path.c_str());
+				delete parser;
+			}
+
+			inline void RunOnString(const std::string content)
+			{
+				xercesc::SAX2XMLReader* parser = xercesc::XMLReaderFactory::createXMLReader();
+				parser->setFeature(xercesc::XMLUni::fgSAX2CoreValidation, true);
+				parser->setFeature(xercesc::XMLUni::fgSAX2CoreNameSpaces, true);   // optional
+				parser->setFeature(xercesc::XMLUni::fgXercesSchema , false);   // optional
+
+				// set up call back handlers
+				WikiDumpXercesHandler handler(PageHandler, HandlerProperties, "[in_memory_file]");
+
+				parser->setContentHandler(&handler);
+				parser->setErrorHandler(&handler);
+
+				xercesc::MemBufInputSource source(reinterpret_cast<const XMLByte*>(content.c_str()), content.length(), "wiki_xml_dump_xerces");
+
+				// run parser
+				parser->parse(source);
 				delete parser;
 			}
 
